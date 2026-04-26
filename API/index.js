f7dbc287a326b2f5e2b6fbc05ef21f5197546ef0
Raghav Vats <raghavmishra98601@gmail.com>
2026-04-26 21:48:25 +0530

    new commit

---
+++ API/index.js
@@ -0,0 +1,46 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const mongoDb = require('mongodb');
+
+
+// To make Executable Function
+const server = express();
+
+// parse requests of content-type - application/json
+server.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+server.use(express.urlencoded({ extended: true }));
+
+server.use(bodyParser.json());
+server.use(cors());
+
+server.get('/',(request,response) => {
+    response.send('<h1>server is working..</h1>');
+})
+
+// Admin URL
+// require('./src/routes/admin-panel/product.routes')(server);
+
+// Application URL
+
+
+//Website URL
+require('./src/routes/website/user.routes.js')(server);
+require('./src/routes/website/order.routes.js')(server);
+
+
+
+server.listen(5000, () => {
+    mongoose.connect('mongodb://127.0.0.1:27017/mongodb_project')
+  .then(() => console.log('Connected!'))
+  .catch((error) => {
+    console.log(error);
+  })
+    console.log('server is working fine...');
+})
+
+
